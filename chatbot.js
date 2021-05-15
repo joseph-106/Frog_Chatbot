74d33de05163d9e48d14ceab91c8caaeb06f8f81
joseph-106 <sjjoseph106@gmail.com>
2021-05-15 12:19:31 +0900

    add footer and img change

--- chatbot.js
+++ chatbot.js
@@ -7,24 +7,28 @@ var data = {
 }
 
 function check_text() {
-	var value = document.getElementById("input").value;
-	var frog = document.getElementsByClassName("frog_says");
+	var value = document.getElementById('input').value;
+	var frog_says = document.getElementsByClassName('frog_says');
+	var frog = document.getElementById('frog');
 
-	if(value == "안녕"){
-		frog[0].innerHTML = data.안녕;
+	frog.src = "static/images/frog1.png";
+
+	if(value == '안녕'){
+		frog_says[0].innerHTML = data.안녕;
 	}
-    if(value == "뭐해"){
-		frog[0].innerHTML = data.뭐해;
+    if(value == '뭐해'){
+		frog_says[0].innerHTML = data.뭐해;
 	}
-    if(value == "잘있어"){;
-		frog[0].innerHTML = data.잘있어;
+    if(value == '잘있어'){;
+		frog_says[0].innerHTML = data.잘있어;
 	}
-	if(value == "불꺼줘"){
+	if(value == '불꺼줘'){
 		document.body.style.backgroundColor = '#819475';
-		frog[0].innerHTML = data.불꺼줘;
+		frog_says[0].innerHTML = data.불꺼줘;
+		frog.src = "static/images/frog2.png";
 	}
-	if(value == "불켜줘"){
+	if(value == '불켜줘'){
 		document.body.style.backgroundColor = '#d2edc0';
-		frog[0].innerHTML = data.불켜줘;
+		frog_says[0].innerHTML = data.불켜줘;
 	}
 }
