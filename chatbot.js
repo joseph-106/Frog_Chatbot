2c7b3a2f64aa1e27ed8836f72446210b19945a8b
joseph-106 <sjjoseph106@gmail.com>
2021-05-18 17:09:31 +0900

    타이핑 애니메이션 적용

--- chatbot.js
+++ chatbot.js
@@ -1,12 +1,17 @@
-var data = {
-	'안녕': '반갑다 개굴!',
-	'뭐해': '개굴개굴거리고 있다!',
-	'잘있어': '잘가라 개굴!',
-	'불꺼줘': '불을 꺼주겠다 개굴!',
-	'불켜줘': '불을 켜주겠다 개굴!',
-	'따라해봐': '개굴! 개굴!',
-	'그만해': '알겠다 개굴..'
+//타이핑 함수
+var app = document.getElementById("frog_console");
+function typing(text) {
+    var typewriter = new Typewriter(app, {
+        loop: false,
+		delay: 70
+      });
+
+      typewriter
+        .typeString(text)
+        .pauseFor(700)
+        .start();
 }
+
 var n = 0; //몇 번째 입력인지 구분하기 위한 변수
 var light = 1; //불이 켜져있는지 구분하기 위한 변수
 var follow = 0; //따라하기 코드를 구분하기 위한 변수
@@ -20,47 +25,45 @@ function check_text() {
 		frog.src = "static/images/frog1.png";
 
 		if(value == '안녕'){
-			reply.innerHTML = data.안녕;
+			typing('반갑다 개굴!')
 		}
     	if(value == '뭐해'){
-			reply.innerHTML = data.뭐해;
+			typing('개굴개굴거리고 있다!')
 		}
     	if(value == '잘있어'){;
-			reply.innerHTML = data.잘있어;
+			typing('잘가라 개굴!')
 		}
 		if(value == '불꺼줘'){
 			if(light == 0){
-				reply.innerHTML = "불은 이미 꺼져있다 개굴!";
+				typing('불은 이미 꺼져있다 개굴!')
 			}
 			else{
 				if (n == 0){
 					n++;
-					reply.innerHTML = "싫은데 개굴?";
+					typing('싫은데 개굴?')
 					frog.src = "static/images/frog2.png";
 				}
 				else if(n == 1){
 					n++;
-					reply.innerHTML = "귀찮다.. 개굴";
+					typing('귀찮다.. 개굴')
 					frog.src = "static/images/frog2.png";
 				}
 				else{
 					light = 0;
 					document.body.style.backgroundColor = '#819475';
-					reply.innerHTML = data.불꺼줘;
+					typing('불을 꺼주겠다 개굴!')
 				}
 			}
-			
 		}
 		if(value == '불켜줘'){
 			if(light == 1){
-				reply.innerHTML = '불은 이미 켜져있다 개굴!';
+				typing('불은 이미 켜져있다 개굴!')
 			}
 			else{
 				light = 1;
 				document.body.style.backgroundColor = '#d2edc0';
-				reply.innerHTML = data.불켜줘;
+				typing('불을 켜주겠다 개굴!')
 			}
-			
 		}
 		if(value == '따라해봐'){
 			follow = 1;
@@ -70,14 +73,14 @@ function check_text() {
 	if(follow == 1){
 		if (value == '그만해'){
 			follow = 0;
-			reply.innerHTML = data.그만해;
+			typing('알겠다 개굴..')
 			frog.src = "static/images/frog2.png";
 		}
 		else if(value == '따라해봐'){
-			reply.innerHTML = data.따라해봐;
+			typing('개굴! 개굴!')
 		}
 		else{
-			reply.innerHTML = value + ' 개굴!';
+			typing(value + ' 개굴!')
 		}
 	}
 }
